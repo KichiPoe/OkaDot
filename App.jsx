6c7ff9cd1128a287ae462a5629d18053d07381ba
Kichi Poe <kishanrp.pc@gmail.com>
2026-07-23 14:45:27 +0530

    chore(jsx): change to jsx project

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import { SafeAreaView, StatusBar, StyleSheet, Text } from 'react-native';
+
+function App() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <StatusBar barStyle="dark-content" />
+            <Text>Hello React Native 👋</Text>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
+
+export default App;
